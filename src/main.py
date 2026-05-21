1bd35c8405ba9070addc654d2a7a6f418a0388c1
jnhghgi <luis.birnbaumer@student.htl-rankweil.at>
2026-05-21 10:08:31 +0000

    Create Boilerplate Code for FastAPI

---
+++ src/main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI
+from pydantic import ValidationError
+from fastapi.exceptions import RequestValidationError
+from fastapi import Request
+from starlette import status
+from starlette.responses import JSONResponse
+
+from database import engine
+import models
+from routers import *
+
+# Erstellt alle Tabellen im Datenbank Schema (falls noch nicht vorhanden)
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="myApp", description="myApp", version="1.0.0")
+
+
+# Custome valdidation Error handler
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    errors = []
+    for error in exc.errors():
+        field = error.get("loc")[-1]
+        erro_msg = error.get("msg")
+        errors.append({"field": field, "msg": erro_msg})
+    return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST, content={"status": "validationerror ", "errors": errors})
+
+
+
+@app.get("/")
+def root():
+    return {"Message": "Hello World\n Besuche /docs für die API"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000)
