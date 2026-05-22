10f1509c01efd03b0be0498931c2eab1dd68a746
jnhghgi <luis.birnbaumer@student.htl-rankweil.at>
2026-05-22 23:10:01 +0000

    Created models for Tag,ShaderTag,Comment,Shader and improved User model

--- src/models.py
+++ src/models.py
@@ -1,10 +1,35 @@
-from sqlalchemy import Column, Integer, String, BOOLEAN, VARCHAR
+from sqlalchemy import Column, Integer, String, BOOLEAN, VARCHAR, ForeignKey
 from database import Base
 
 
 class DBUsers(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
-    name = Column(VARCHAR, index=True)
+    __tablename__ = "Users"
+    UserId = Column(Integer, primary_key=True)
+    UserName = Column(String, max_lengh=31 , unique=True ,index=True)
     passwd = Column(String)
+
+
+class DBShader(Base):
+    __tablename__ = "Shaders"
+    ShaderId = Column(Integer, primary_key=True)
+    ShaderCode = Column(String, index=True)
+
+
+class DBComment(Base):
+    __tablename__ = "Comment"
+    CommentId = Column(Integer, primary_key=True)
+    CommentText = Column(String, max_length=255)
+    user_id = Column(Integer, ForeignKey("Users.UserId"))
+
+class ShaderTags(Base):
+    __tablename__ = "ShaderTags"
+    shaderId = Column(Integer, ForeignKey("Shaders.ShaderId"))
+    tagId = Column(Integer, ForeignKey("Tags.TagId"))
+
+class Tags(Base):
+    __tablename__ = "Tags"
+    TagId = Column(Integer, primary_key=True)
+    TagName = Column(String,max_legnth=31 ,unique=True, index=True)
+
+
 
